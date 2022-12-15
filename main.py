4c88b95d3eaa802322b10a28dc09bd0ccd2cb595
igalvaoq <igor.quaresma@uni-muenster.de>
2022-12-15 13:11:34 +0100

    map view with Freiburg city limits

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return '<h1>Hello World</h1>'
 
-app.run()
+def root():
+    markers = [
+        {
+            'lat': 47.9990,
+            'lon': 7.8421,
+            'popup': 'Freiburg im Breisgau'
+        }
+    ]
+    return render_template('index.html', markers=markers)
+
+
+if __name__ == '__main__':
+   app.run(host="localhost", port=8080, debug=True)
