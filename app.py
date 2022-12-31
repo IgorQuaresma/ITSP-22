4e102987782bfea9b3b6f41f3797e759e26c673d
igalvaoq <igor.quaresma@uni-muenster.de>
2022-12-31 02:00:00 +0100

    plotting all the bus stops markers in a new route

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/')
 
-def city1():
+def city_center():
     markers = [
         {
             'lat': 47.9990,
@@ -14,13 +15,12 @@ def city1():
     ]
     return render_template('index.html', markers=markers)
 
-@app.route('/bus_stops')
-def bus_stops():
-    bus_stops = [{'lat': 48.023901, 'lon': 7.724595, 'popup': 'Schutternstraße A'},
-                 {'lat': 48.023775, 'lon': 7.724460, 'popup': 'Schutternstraße B'},
-                 {'lat': 48.023414, 'lon': 7.719223, 'popup': 'Waltershofen Ochsen'}]
+@app.route('/bus_stops_all')
+def bus_stops_all():
+    df_all_stops = pd.read_csv("./data/gtfs/stops.txt")
+    dict_all_stops = df_all_stops.to_dict('records')
 
-    return render_template('index.html', bus_stops_markers=bus_stops)
+    return render_template('index.html', bus_stops_all_markers=dict_all_stops)
 
 if __name__ == '__main__':
    app.run(host="localhost", port=8080, debug=True)
