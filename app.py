a91d15ecf52dd9bc04dea34aa235c6214492c419
igalvaoq <igor.quaresma@uni-muenster.de>
2023-02-18 18:27:55 +0100

    cleaning stuff

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect, session
 import pandas as pd
+import folium
 
 app = Flask(__name__)
+app.secret_key = "12345"
 
 @app.route('/')
 def bus_stops_all():
@@ -11,9 +13,11 @@ def bus_stops_all():
 
 
     df_combined = df_combined[df_combined["stop_name"].str.contains("Freiburg")] #filter only stops in Freiburg (optimize performance)
-    dict_all_stops = df_all_stops.to_dict('records')
+    dict_all_stops = df_combined.to_dict('records')
 
-    return render_template('home.html', bus_stops_all_markers=dict_all_stops)
+    test = 2
+
+    return render_template('home.html', bus_stops_all_markers=dict_all_stops, test=test)
 
 @app.route('/stop_times')
 def stop_times():
@@ -26,20 +30,20 @@ def stop_times():
 
     return render_template('home.html', bus_stops_all_markers=dict_combined)
 
-@app.route('/city_center')
-def city_center():
-    markers = [
-        {
-            'lat': 47.9990,
-            'lon': 7.8421,
-            'popup': 'Freiburg im Breisgau'
-        }
-    ]
-    return render_template('home.html', markers=markers)
 
 @app.route('/about/')
 def about():
     return render_template('about.html')
+
+@app.route('/process_form', methods=['POST'])
+def process_form():
+    form_input = request.form['input_field']
+    session['form_input'] = form_input
+    # process form input
+    # ...
+    print(form_input)
+    return redirect(url_for('bus_stops_all', form_input = form_input))
+
 
 
 if __name__ == '__main__':
