bd0e4c31ae404a4c60073740c666b15b7d580bae
igalvaoq <igor.quaresma@uni-muenster.de>
2023-02-19 02:25:07 +0100

    info cards before merge request

--- app.py
+++ app.py
@@ -11,23 +11,13 @@ def bus_stops_all():
 
 
     df_combined = df_combined[df_combined["stop_name"].str.contains("Freiburg")] #filter only stops in Freiburg (optimize performance)
-    df_combined = df_combined.drop_duplicates(subset=['stop_id'])
+    df_combined = df_combined.drop_duplicates(subset=['stop_id']) #remove duplicates considering only stop_id as reference
 
     dict_all_stops = df_combined.to_dict('records')
 
 
     return render_template('home.html', bus_stops_all_markers=dict_all_stops)
 
-@app.route('/stop_times')
-def stop_times():
-    df_all_stops = pd.read_csv("./data/gtfs/stops.txt")
-    df_stop_times = pd.read_csv("./data/gtfs/stop_times.txt")
-    df_combined = pd.merge(df_all_stops, df_stop_times, left_on="stop_id", right_on="stop_id") #combine both dataframes using "stop_id" column
-
-    df_combined = df_combined[df_combined["stop_name"].str.contains("Freiburg")] #filter only stops in Freiburg (optimize performance)
-    dict_combined = df_combined.to_dict('records')
-
-    return render_template('home.html', bus_stops_all_markers=dict_combined)
 
 @app.route('/city_center')
 def city_center():
