b2630f145cc3c8d20e46b1ca25d468a412e70456
igalvaoq <igor.quaresma@uni-muenster.de>
2023-02-28 23:49:36 +0100

    FINAL COMMIT

--- static/js/universal.js
+++ static/js/universal.js
@@ -15,19 +15,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 var marker = L.marker([47.9990, 7.8421]).addTo(map);
 
 
-// Define a function to get the user's location and add a marker to the map
-function getUserLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      // Create a marker for the user's location and add it to the map
-      var userMarker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
-
-      // Center the map on the user's location
-      map.setView([position.coords.latitude, position.coords.longitude], 13);
-    });
-  }
-}
-
 // Add a button to the HTML page and add a get the user location when clicked
 function showUserLocation() {
     if (navigator.geolocation) {
